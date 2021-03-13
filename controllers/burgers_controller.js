2b3ef7138c5efc034d9691b5fb319b46ccf00e40
Kat Buchanan <katrbuch@gmail.com>
2021-03-13 15:35:53 -0600

    post one

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -12,4 +12,20 @@ router.get('/', (req, res) => {
     });
   });
 
+  router.post('/api/burgers', (req, res) => {
+    let devoured = 0;
+    if (req.body.devoured === 'true') {
+      devoured = 1;
+    }
+    burger.insert(
+      ['burger_name', 'devoured'],
+      [req.body.burger_name, devoured],
+      (result) => {
+        console.log(result);
+        res.json({ id: result.insertId });
+      }
+    );
+  });
+  
+
 module.exports = router;
