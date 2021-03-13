e65852879cb3df73f74d82aa95007c27e1bb3b15
Kat Buchanan <katrbuch@gmail.com>
2021-03-13 15:34:18 -0600

    get all

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,4 +2,14 @@ const express = require('express');
 const router = express.Router();
 const burger = require('../models/burger.js');
 
+router.get('/', (req, res) => {
+    burger.all((data) => {
+      const hbsObject = {
+        burgers: data,
+      };
+      console.log(hbsObject);
+      res.render('index', hbsObject);
+    });
+  });
+
 module.exports = router;
