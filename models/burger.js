c73abc6e6d55f9fe58d7e19d1fee8fd88aa89ddf
Kat Buchanan <katrbuch@gmail.com>
2021-03-13 15:30:18 -0600

    crud functions

--- models/burger.js
+++ models/burger.js
@@ -1,3 +1,26 @@
 const orm = require('../config/orm.js');
 
+const burgers = {
+    all(cb) {
+      orm.all('burgers', (res) => {
+        cb(res);
+      });
+    },
+    insert(cols, vals, cb) {
+      orm.insert('burgers', cols, vals, (res) => {
+        cb(res);
+      });
+    },
+    update(objColVals, condition, cb) {
+      orm.update('burgers', objColVals, condition, (res) => {
+        cb(res);
+      });
+    },
+    delete(condition, cb) {
+      orm.delete('burgers', condition, (res) => {
+        cb(res);
+      });
+    },
+  };
+
 module.exports = burgers;
