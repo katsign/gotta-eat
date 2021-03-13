8b9616186166ea14217945349d74678a2afc95b8
Kat Buchanan <katrbuch@gmail.com>
2021-03-13 14:43:12 -0600

    db init

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO burgers_db.burgers (burger_name, devoured)
+VALUES
+("Teriyaki Soy Burger", false),
+("Portobello Swiss Burger", false),
+("Black Bean Burger", false),
+("Quinoa Burger", false),
+("Impossible Burger", false);
